c62e361dc582e4376cb838585f4dcc65ee4e7a2d
Frank Naegler <frank.naegler@typo3.org>
2016-10-04 15:36:32 +0200

    [TASK] Cleanup ext_localconf.php and ext_tables.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,123 +3,127 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'Posts',
-    [
-        'Post' => 'listRecentPosts',
-    ]
-);
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'Category',
-    [
-        'Post' => 'listPostsByCategory',
-    ],
-    [
-        'Post' => 'listPostsByCategory',
-    ]
-);
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'Tag',
-    [
-        'Post' => 'listPostsByTag',
-    ]
-);
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'Archive',
-    [
-        'Post' => 'listPostsByDate',
-    ],
-    [
-        'Post' => 'listPostsByDate',
-    ]
-);
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'Sidebar',
-    [
-        'Post' => 'sidebar',
-    ]
-);
+call_user_func(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'Posts',
+        [
+            'Post' => 'listRecentPosts',
+        ]
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'Category',
+        [
+            'Post' => 'listPostsByCategory',
+        ],
+        [
+            'Post' => 'listPostsByCategory',
+        ]
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'Tag',
+        [
+            'Post' => 'listPostsByTag',
+        ]
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'Archive',
+        [
+            'Post' => 'listPostsByDate',
+        ],
+        [
+            'Post' => 'listPostsByDate',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'CommentForm',
-    [
-        'Comment' => 'form, addComment',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'Sidebar',
+        [
+            'Post' => 'sidebar',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'Comments',
-    [
-        'Comment' => 'comments',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'CommentForm',
+        [
+            'Comment' => 'form, addComment',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'Meta',
-    [
-        'Post' => 'metadata',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'Comments',
+        [
+            'Comment' => 'comments',
+        ]
+    );
 
-// Widgets
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'RecentPostsWidget',
-    [
-        'Widget' => 'recentPosts',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'Meta',
+        [
+            'Post' => 'metadata',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'CategoryWidget',
-    [
-        'Widget' => 'categories',
-    ]
-);
+    // Widgets
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'RecentPostsWidget',
+        [
+            'Widget' => 'recentPosts',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'TagWidget',
-    [
-        'Widget' => 'tags',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'CategoryWidget',
+        [
+            'Widget' => 'categories',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'CommentsWidget',
-    [
-        'Widget' => 'comments',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'TagWidget',
+        [
+            'Widget' => 'tags',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'ArchiveWidget',
-    [
-        'Widget' => 'archive',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'CommentsWidget',
+        [
+            'Widget' => 'comments',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'T3G.AgencyPack.Blog',
-    'FeedWidget',
-    [
-        'Widget' => 'feed',
-    ]
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'ArchiveWidget',
+        [
+            'Widget' => 'archive',
+        ]
+    );
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['blog'] =
-        \T3G\AgencyPack\Blog\Hooks\RealUrlAutoConfiguration::class . '->addBlogConfiguration';
-}
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'T3G.AgencyPack.Blog',
+        'FeedWidget',
+        [
+            'Widget' => 'feed',
+        ]
+    );
+
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['blog']
+            = \T3G\AgencyPack\Blog\Hooks\RealUrlAutoConfiguration::class .
+            '->addBlogConfiguration';
+    }
+});
--- ext_tables.php
+++ ext_tables.php
@@ -1,101 +1,103 @@
 <?php
 
-call_user_func(
-    function () {
-        $blogDocType = \T3G\AgencyPack\Blog\Constants::DOKTYPE_BLOG_POST;
+if (!defined('TYPO3_MODE')) {
+    die('Access denied!');
+}
 
-        // Add new page type:
-        $GLOBALS['PAGES_TYPES'][$blogDocType] = [
-            'type' => 'web',
-            'allowedTables' => '*',
-        ];
+call_user_func(function () {
+    $blogDocType = \T3G\AgencyPack\Blog\Constants::DOKTYPE_BLOG_POST;
 
-        // Provide icon for page tree, list view, ... :
-        $icons = [
-            'blog-link-wizard' => 'EXT:blog/Resources/Public/Icons/blog-link-wizard.svg',
-            'apps-pagetree-folder-contains-blog' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-folder-contains-blog.svg',
-            'apps-pagetree-blog' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog.svg',
-            'apps-pagetree-blog-category' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-category.svg',
-            'apps-pagetree-blog-comment' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-comment.svg',
-            'apps-pagetree-blog-comment-approved' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-comment-approved.svg',
-            'apps-pagetree-blog-comment-blocked' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-comment-blocked.svg',
-            'apps-pagetree-blog-comment-todo' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-comment-todo.svg',
-            'apps-pagetree-blog-post' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-post.svg',
-            'apps-pagetree-blog-tag' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-tag.svg',
-        ];
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-        foreach ($icons as $identifier => $path) {
-            $iconRegistry->registerIcon(
-                $identifier,
-                TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-                ['source' => $path]
-            );
-        }
+    // Add new page type:
+    $GLOBALS['PAGES_TYPES'][$blogDocType] = [
+        'type' => 'web',
+        'allowedTables' => '*',
+    ];
 
-        // Allow backend users to drag and drop the new page type:
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-            'options.pageTree.doktypesToShowInNewPageDragArea := addToList(' . $blogDocType . ')'
+    // Provide icon for page tree, list view, ... :
+    $icons = [
+        'blog-link-wizard' => 'EXT:blog/Resources/Public/Icons/blog-link-wizard.svg',
+        'apps-pagetree-folder-contains-blog' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-folder-contains-blog.svg',
+        'apps-pagetree-blog' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog.svg',
+        'apps-pagetree-blog-category' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-category.svg',
+        'apps-pagetree-blog-comment' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-comment.svg',
+        'apps-pagetree-blog-comment-approved' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-comment-approved.svg',
+        'apps-pagetree-blog-comment-blocked' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-comment-blocked.svg',
+        'apps-pagetree-blog-comment-todo' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-comment-todo.svg',
+        'apps-pagetree-blog-post' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-post.svg',
+        'apps-pagetree-blog-tag' => 'EXT:blog/Resources/Public/Icons/apps-pagetree-blog-tag.svg',
+    ];
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    foreach ($icons as $identifier => $path) {
+        $iconRegistry->registerIcon(
+            $identifier,
+            TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => $path]
         );
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_blog_domain_model_comment');
-
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'T3G.AgencyPack.Blog',
-            'Posts',
-            'Blog: List of posts'
-        );
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'T3G.AgencyPack.Blog',
-            'Category',
-            'Blog: List by category'
-        );
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'T3G.AgencyPack.Blog',
-            'Tag',
-            'Blog: List by tags'
-        );
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'T3G.AgencyPack.Blog',
-            'Archive',
-            'Blog: Archive'
-        );
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'T3G.AgencyPack.Blog',
-            'Sidebar',
-            'Blog: Sidebar'
-        );
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'T3G.AgencyPack.Blog',
-            'Metadata',
-            'Blog: Metadata'
-        );
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'T3G.AgencyPack.Blog',
-            'CommentForm',
-            'Blog: Comment form'
-        );
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'T3G.AgencyPack.Blog',
-            'Comments',
-            'Blog: Comments'
-        );
-
-        if (TYPO3_MODE === 'BE') {
-            // Module System > Backend Users
-            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-                'T3G.AgencyPack.Blog',
-                'system',
-                'tx_Blog',
-                'top',
-                [
-                    'Backend' => 'index, createBlog'
-                ],
-                [
-                    'access' => 'admin',
-                    'icon' => 'EXT:blog/Resources/Public/Icons/module-blog.svg',
-                    'labels' => 'LLL:EXT:blog/Resources/Private/Language/locallang_mod.xlf'
-                ]
-            );
-        }
     }
-);
+
+    // Allow backend users to drag and drop the new page type:
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+        'options.pageTree.doktypesToShowInNewPageDragArea := addToList(' . $blogDocType . ')'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_blog_domain_model_comment');
+
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'T3G.AgencyPack.Blog',
+        'Posts',
+        'Blog: List of posts'
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'T3G.AgencyPack.Blog',
+        'Category',
+        'Blog: List by category'
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'T3G.AgencyPack.Blog',
+        'Tag',
+        'Blog: List by tags'
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'T3G.AgencyPack.Blog',
+        'Archive',
+        'Blog: Archive'
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'T3G.AgencyPack.Blog',
+        'Sidebar',
+        'Blog: Sidebar'
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'T3G.AgencyPack.Blog',
+        'Metadata',
+        'Blog: Metadata'
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'T3G.AgencyPack.Blog',
+        'CommentForm',
+        'Blog: Comment form'
+    );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'T3G.AgencyPack.Blog',
+        'Comments',
+        'Blog: Comments'
+    );
+
+    if (TYPO3_MODE === 'BE') {
+        // Module System > Backend Users
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'T3G.AgencyPack.Blog',
+            'system',
+            'tx_Blog',
+            'top',
+            [
+                'Backend' => 'index, createBlog'
+            ],
+            [
+                'access' => 'admin',
+                'icon' => 'EXT:blog/Resources/Public/Icons/module-blog.svg',
+                'labels' => 'LLL:EXT:blog/Resources/Private/Language/locallang_mod.xlf'
+            ]
+        );
+    }
+});
