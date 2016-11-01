61bb5f83fe2a91b65e8942436448c79983cfe06f
Frank Naegler <frank.naegler@typo3.org>
2016-11-01 14:38:24 +0100

    [TASK] Add TYPO3_MODE check to all global scope files

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,4 +1,7 @@
 <?php
+
+defined('TYPO3_MODE') or die();
+
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['blog_archive'] = 'recursive,select_key,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['blog_commentform'] = 'recursive,select_key,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['blog_comments'] = 'recursive,select_key,pages';
