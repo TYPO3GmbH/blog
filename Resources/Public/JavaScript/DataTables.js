07200d29f0e3f1a9e4919e1f496bb0db2c46074b
Frank Naegler <frank.naegler@typo3.org>
2017-01-12 16:29:09 +0100

    [FEATURE] Add custom filter per column

--- Resources/Public/JavaScript/DataTables.js
+++ Resources/Public/JavaScript/DataTables.js
@@ -27,7 +27,47 @@ define(['jquery', 'datatables', 'datatables_bootstrap'], function ($) {
                 "columns": function() {
                     return $(this).data('columns')
                 },
-                "pageLength": 10
+                "pageLength": 10,
+                "initComplete": function () {
+                    this.api().columns().every( function () {
+                        var column = this;
+                        if ($(column.header()).data('filter') === true) {
+                            var select = $('<select><option value="">' + $(column.header()).text() + '</option></select>')
+                                .appendTo( $(column.header()).empty() )
+                                .on('click', function(e) {
+                                    e.stopPropagation();
+                                })
+                                .on( 'change', function () {
+                                    var val = $.fn.dataTable.util.escapeRegex(
+                                        $(this).val()
+                                    );
+                                    column
+                                        .search( val ? val : '', true, false )
+                                        .draw();
+                                } );
+                            var values = [];
+                            column.data().each(function(d, j) {
+                                var $object = $(d);
+                                if ($object.find('li').length > 0) {
+                                    $object.find('li').each(function() {
+                                        var str = $(this).text().trim();
+                                        if (str !== '') {
+                                            values.push(str);
+                                        }
+                                    });
+                                } else {
+                                    if (d !== '') {
+                                        values.push(d);
+                                    }
+                                }
+                            });
+                            $($.uniqueSort(values)).each(function() {
+                                var value = this;
+                                select.append( '<option value="'+value+'">'+value+'</option>' )
+                            } );
+                        }
+                    } );
+                }
             });
         });
     };
