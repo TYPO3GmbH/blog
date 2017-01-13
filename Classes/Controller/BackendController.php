9b403ee08c2ae3ae92355bc127017e0e97dd729a
Frank Nägler <frank.naegler@typo3.com>
2017-01-13 13:37:37 +0100

    Update Classes/Controller/BackendController.php

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -25,7 +25,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
 /**
