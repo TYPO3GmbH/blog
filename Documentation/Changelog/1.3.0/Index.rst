c2f21b1387f8399349b1407e59ca5c7b9175f13a
Frank Naegler <frank.naegler@typo3.org>
2017-04-07 17:29:41 +0200

    [TASK] Prepare release 1.3.0

---
+++ Documentation/Changelog/1.3.0/Index.rst
@@ -0,0 +1,41 @@
+
+.. include:: ../../Includes.txt
+
+Changes in version 1.3.0
+========================
+
+**Table of contents**
+
+.. contents::
+:local:
+   :depth: 1
+
+Breaking Changes
+^^^^^^^^^^^^^^^^
+
+.. toctree::
+:maxdepth: 1
+      :titlesonly:
+      :glob:
+
+      Breaking-*
+
+Features
+^^^^^^^^
+
+.. toctree::
+:maxdepth: 1
+      :titlesonly:
+      :glob:
+
+      Feature-*
+
+Deprecation
+^^^^^^^^^^^
+
+.. toctree::
+:maxdepth: 1
+      :titlesonly:
+      :glob:
+
+      Deprecation-*
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 Blog Extension',
-    'description' => 'Blogging with TYPO3',
+    'description' => 'This blog extension use TYPO3s core concepts and elements to provide a full-blown blog that users of TYPO3 can instantly understand and use.',
     'category' => 'fe',
     'state' => 'stable',
     'uploadfolder' => 0,
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 0,
     'author' => 'TYPO3 GmbH',
     'author_email' => 'info@typo3.com',
-    'version' => '1.3.0-dev',
+    'version' => '1.3.0',
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-8.7.99',
