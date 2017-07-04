ac443105a0c70bab0a26080f30687635e079f40f
Frank Naegler <frank.naegler@typo3.org>
2017-07-04 11:35:54 +0200

    [TASK] Adjust version contraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '7.6.x-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-7.6.99',
         ],
         'conflicts' => [],
         'suggests' => [],
