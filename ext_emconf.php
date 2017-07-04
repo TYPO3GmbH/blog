6d86fa2b05cf4f922852af6bd03b1fff38968577
Frank Naegler <frank.naegler@typo3.org>
2017-07-04 09:13:10 +0200

    [TASK] Raise core version requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.4.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '9.0.0-9.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
