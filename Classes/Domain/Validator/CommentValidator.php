1a9170d0cf3f8b0275f90279c0c66489e2fc5879
Frank Nägler <frank.naegler@typo3.com>
2017-07-31 10:54:02 +0200

    Merge pull request #104 in EXT/blog from feature/EXTBLOG-95-add-google-re-captcha-support to master
    Squashed commit of the following:
    
    commit c2463d33b7d8f578abb7e627212ba993199febcc
    Author: Frank Naegler <frank.naegler@typo3.org>
    Date:   Sat Jul 29 17:52:24 2017 +0200
    
        [FEATURE] Add google re-captcha to comment form
    
        Resolves: EXTBLOG-95
        Releases: master, 8.7

--- Classes/Domain/Validator/CommentValidator.php
+++ Classes/Domain/Validator/CommentValidator.php
@@ -16,6 +16,8 @@ namespace T3G\AgencyPack\Blog\Domain\Validator;
  */
 use T3G\AgencyPack\Blog\Domain\Model\Comment;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator;
 
 /**
@@ -27,6 +29,8 @@ class CommentValidator extends AbstractValidator
      * Checks if the given comment is valid.
      *
      * @param mixed $value The comment model
+     *
+     * @throws \InvalidArgumentException
      */
     public function isValid($value)
     {
@@ -48,6 +52,36 @@ class CommentValidator extends AbstractValidator
             }
             if (trim($value->getUrl()) !== '' && !GeneralUtility::isValidUrl(trim($value->getUrl()))) {
                 $this->addError('The url has an invalid format', 1467650568);
+            }
+
+            $settings = GeneralUtility::makeInstance(ObjectManager::class)
+                ->get(ConfigurationManagerInterface::class)
+                ->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS, 'blog');
+            if ((int)$settings['comments']['google_recaptcha']['_typoScriptNodeValue'] === 1) {
+                // this validator is called multiple times, if the first success,
+                // the global variable is set, else validate the re-captcha
+                if (empty($GLOBALS['google_recaptcha'])) {
+                    $post_data = http_build_query([
+                        'secret' => $settings['comments']['google_recaptcha']['secret_key'],
+                        'response' => GeneralUtility::_GP('g-recaptcha-response'),
+                        'remoteip' => $_SERVER['REMOTE_ADDR']
+                    ]);
+                    $opts = [
+                        'http' => [
+                            'method'  => 'POST',
+                            'header'  => 'Content-type: application/x-www-form-urlencoded',
+                            'content' => $post_data
+                        ]
+                    ];
+                    $context  = stream_context_create($opts);
+                    $response = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+                    $result = json_decode($response);
+                    if (!$result->success) {
+                        $this->addError('The re-captcha failed', 1501341100);
+                    } else {
+                        $GLOBALS['google_recaptcha'] = true;
+                    }
+                }
             }
         }
     }
---
+++ Documentation/Changelog/master/Feature-95-Add-Google-Re-Captcha.rst
@@ -0,0 +1,31 @@
+.. include:: ../../Includes.txt
+
+============================================
+Feature: #EXTBLOG-95 - Add Google Re-Captcha
+============================================
+
+See https://jira.typo3.com/browse/EXTBLOG-95
+
+Description
+===========
+
+The google re-captcha was added to the comment form but have to be activated and configured.
+The feature prevent comment spam by adding an additional captcha field to the comment form.
+To use the feature it is required to activate and configure the captcha field.
+The required keys can be received from google: https://www.google.com/recaptcha/admin#list
+
+.. code-block::typoscript
+
+   plugin.tx_blog.settings.google_recaptcha = 1
+   plugin.tx_blog.settings.google_recaptcha {
+      website_key = 6LfN-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+      secret_key = 6LfN-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+   }
+
+
+Impact
+======
+
+You can now reduce the amount of spam comments by activate this feature.
+
+.. index:: TypoScript, Frontend
