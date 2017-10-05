eb5baadf882b7659580d0855de4e0bbee5970b86
Benjamin Kott <benjamin.kott@wfp2.com>
2017-10-05 16:01:11 +0200

    [TASK] Make canvas responsive

--- Classes/Form/Wizards/SocialImageWizardController.php
+++ Classes/Form/Wizards/SocialImageWizardController.php
@@ -33,25 +33,69 @@ class SocialImageWizardController
     public function indexAction(ServerRequestInterface $request, ResponseInterface $response)
     {
         $markup = '
-        
-        <div class="container">
-            <input type="text" placeholder="Enter Social Text here">
+        <style>
+            .preview-container {
+                overflow: hidden;
+            }
+            .preview {
+                background-color: #515151;
+                margin-bottom: 1.5em;
+                width: 100%;
+                position: relative;
+            }
+            .preview:before {
+                display: block;
+                content: "";
+            }
+            .preview canvas {
+                position: absolute;
+                top: 0;
+                left: 0;
+            }
+            .preview-facebook {
+                max-width: 512px;
+            }
+            .preview-facebook:before {
+                padding-top: 50%;
+            }
+            .preview-linkedin {
+                max-width: 350px;
+            }
+            .preview-linkedin:before {
+                padding-top: 71.42857142857143%;
+            }
+            .preview-googleplus {
+                max-width: 426px;
+            }
+            .preview-googleplus:before {
+                padding-top: 50%;
+            }
+        </style>
+        <div class="preview-container">
+            <input type="text" class="form-control" placeholder="Enter Social Text here">
         </div>
-        <div class="container-fluid">
-            <div class="col-sm-12 col-lg-4 preview-facebook">
-                <h3>Facebook / Twitter</h3>
-                <canvas id="preview-facebook" width="512" height="256" style="border: 1px solid grey"></canvas>
-            </div>
-            <div class="col-sm-12 col-lg-2 preview-linkedin">
-                <h3>LinkedIn</h3>
-                <canvas id="preview-linkedin" width="350" height="250" style="border: 1px solid grey"></canvas>
-            </div>
-            <div class="col-sm-12 col-lg-3 preview-googleplus">
-                <h3>Google+</h3>
-                <canvas id="preview-googleplus" width="426" height="213" style="border: 1px solid grey"></canvas>
+        <div class="preview-container">
+            <div class="row">
+                <div class="col-sm-12 col-md-4">
+                    <h3>Facebook / Twitter (512x256)</h3>
+                    <div class="preview preview-facebook">
+                        <canvas id="preview-facebook" width="512" height="256"></canvas>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <h3>LinkedIn (350x250)</h3>
+                    <div class="preview preview-linkedin">
+                        <canvas id="preview-linkedin" width="350" height="250"></canvas>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <h3>Google+ (426x213)</h3>
+                    <div class="preview preview-googleplus">
+                        <canvas id="preview-googleplus" width="426" height="213"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
-        
         ';
 
         $response->getBody()->write($markup);
