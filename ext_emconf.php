4a4dd462717ade25dbd00051e2a11982162bb79b
Frank Naegler <frank.naegler@typo3.org>
2017-11-10 09:35:50 +0100

    [BUGFIX] Fix typo in description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 Blog Extension',
-    'description' => 'This blog extension use TYPO3s core concepts and elements to provide a full-blown blog that users of TYPO3 can instantly understand and use.',
+    'description' => 'This blog extension uses TYPO3s core concepts and elements to provide a full-blown blog that users of TYPO3 can instantly understand and use.',
     'category' => 'fe',
     'state' => 'stable',
     'uploadfolder' => 0,
