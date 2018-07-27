193aabddad158c5e852376267e88eb5a73a9a859
Frank Naegler <frank.naegler@typo3.org>
2018-07-27 13:54:18 +0200

    [TASK] bamboo setup

---
+++ Build/php_versions.sh
@@ -0,0 +1,11 @@
+PHP_VERSIONS=('php72')
+
+# v8
+# PHP_VERSIONS+=('php71')
+# PHP_VERSIONS+=('php70')
+
+# v7
+# PHP_VERSIONS+=('php71')
+# PHP_VERSIONS+=('php70')
+# PHP_VERSIONS+=('php56')
+# PHP_VERSIONS+=('php55')
