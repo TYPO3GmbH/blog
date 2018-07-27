c3b13e782d55e0492ea2e8cda4b40d09fec6ee28
Frank Naegler <frank.naegler@typo3.org>
2018-07-27 13:54:18 +0200

    [TASK] bamboo setup
    # Conflicts:
    #	composer.json

---
+++ Build/bamboo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+if [ "$(ps -p "$$" -o comm=)" != "bash" ]; then
+    bash "$0" "$@"
+    exit "$?"
+fi
+
+# fail immediately if some command failed
+set -e
+# output all commands
+set -x
+
+# Create log directory
+mkdir -p logs
+
+# lint, phpunit, composer in docker helper functions
+source Build/bamboo-container-functions.sh
+
+# load php version to test
+source Build/php_versions.sh
+
+for TEST_PHP_VERSION in "${PHP_VERSIONS[@]}"; do
+    # Check for PHP Errors
+    runLint
+
+    # Composer install dependencies using docker function
+    runComposer install --no-interaction --no-progress
+
+    # CGL Checks
+    # Disabled for now since php-cs-fixer is not available
+    runPhpCsFixer fix --config Build/.php_cs.dist --format=junit > logs/php-cs-fixer.xmlcomposer
+
+    # Unit tests
+    runPhpunit -c Build/UnitTests.xml --log-junit logs/phpunit.xml
+
+    # Functional tests
+    # runPhpunit -c Build/FunctionalTests.xml --log-junit logs/functional.xml
+done
