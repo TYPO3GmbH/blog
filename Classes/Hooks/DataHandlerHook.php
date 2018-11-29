0f51f9705b2e23b61b5c73b72f9088ed6f1e766c
Frank Nägler <frank.naegler@typo3.com>
2018-11-29 14:20:50 +0100

    [BUGFIX] Cast timestamp to int Releases: master, 9.0, 8.7
    Squashed commit of the following:
    
    commit 5d7d5ffbbdb49d2a961f3b605ff3f8be1809ba9d
    Author: Romain Canon <romain.hydrocanon@gmail.com>
    Date:   Thu Nov 29 11:32:19 2018 +0100
    
        [BUGFIX] Cast timestamp to int
    
        The timestamp fetched from a record can be a string, where the function
        `date()` requires an `int`.
    
        This commit prevents a fatal error being thrown when the timestamp is a
        string.

--- Classes/Hooks/DataHandlerHook.php
+++ Classes/Hooks/DataHandlerHook.php
@@ -58,8 +58,8 @@ class DataHandlerHook
                 $queryBuilder
                     ->update($table)
                     ->set('publish_date', $record['publish_date'])
-                    ->set('crdate_month', date('n', $timestamp))
-                    ->set('crdate_year', date('Y', $timestamp))
+                    ->set('crdate_month', date('n', (int)$timestamp))
+                    ->set('crdate_year', date('Y', (int)$timestamp))
                     ->where($queryBuilder->expr()->eq('uid', (int)$id))
                     ->execute();
             }
