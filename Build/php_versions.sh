7407536a97caf05360ea7f31d6ea8f12792f2235
Frank Naegler <frank.naegler@typo3.org>
2019-01-17 15:41:52 +0100

    Revert "[BUGFIX] Disable tests with PHP 7.1"
    This reverts commit 45d9b9099dbd693c4cb6d6e14ba8d9fe371eee7f.

--- Build/php_versions.sh
+++ Build/php_versions.sh
@@ -1,8 +1,7 @@
 PHP_VERSIONS=('php72')
 
 # v8
-# PHP 7.1 disabled because of incompatible dependencies with cs-fixer and symfony/polyfill-php70
-# PHP_VERSIONS+=('php71')
+PHP_VERSIONS+=('php71')
 PHP_VERSIONS+=('php70')
 
 # v7
