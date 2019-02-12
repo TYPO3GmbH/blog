cd4c7e408d84b962493f6f56f5851b6fe47d828a
Benjamin Kott <benjamin.kott@outlook.com>
2019-02-12 15:15:27 +0100

    [BUGFIX] LinkViewHelper must set correct controller context (#28)

--- Classes/ViewHelpers/Link/AuthorViewHelper.php
+++ Classes/ViewHelpers/Link/AuthorViewHelper.php
@@ -73,17 +73,11 @@ class AuthorViewHelper extends AbstractTagBasedViewHelper
      */
     protected function buildUriFromDefaultPage(Author $author, bool $rssFormat)
     {
-        $uriBuilder = $this->getUriBuilder(
-            (int)$this->getTypoScriptFrontendController()->tmpl->setup['plugin.']['tx_blog.']['settings.']['authorUid'],
-            [
-                'tx_blog_authorposts' => [
-                    'author' => $author->getUid(),
-                ],
-            ],
-            $rssFormat
-        );
-
-        return $this->buildAnchorTag($uriBuilder->uriFor('listPostsByAuthor', [], 'Post'), $author, 'Blog');
+        $uriBuilder = $this->getUriBuilder((int)$this->getTypoScriptFrontendController()->tmpl->setup['plugin.']['tx_blog.']['settings.']['authorUid'], [], $rssFormat);
+        $arguments = [
+            'author' => $author->getUid(),
+        ];
+        return $this->buildAnchorTag($uriBuilder->uriFor('listPostsByAuthor', $arguments, 'Post', 'Blog', 'AuthorPosts'), $author);
     }
 
     /**
