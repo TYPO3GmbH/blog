35218dcb4633f87453c799a999730dd34a5bd5a6
Benjamin Kott <benjamin.kott@outlook.com>
2019-02-14 17:44:02 +0100

    [BUGFIX] Resolve endless loop when calling archive without params (#35)
    Releases: master, 9.1, 9.0
    Fixes: #27

---
+++ Classes/Utility/ArchiveUtility.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package t3g/blog.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\AgencyPack\Blog\Utility;
+
+class ArchiveUtility
+{
+    /**
+     * This method extracts and sorts the database result and create a nested array
+     * in the form:
+     * [
+     *  2015 => [
+     *    [
+     *      'year' => 2015,
+     *      'month' => 3,
+     *      'count' => 9
+     *      'timestamp' => 123456789
+     *    ]
+     *    ...
+     *  ]
+     *  ...
+     * ]
+     *
+     * @param array $data
+     * @return array
+     * @throws \Exception
+     */
+    public static function extractDataFromPosts(array $data): array
+    {
+        $archiveData = [];
+        foreach ($data as $result) {
+            if (empty($archiveData[$result['year']])) {
+                $archiveData[$result['year']] = [];
+            }
+            $dateTime = new \DateTimeImmutable(sprintf('%d-%d-1', (int)$result['year'], (int)$result['month']));
+            $result['timestamp'] = $dateTime->getTimestamp();
+            $archiveData[$result['year']][] = $result;
+        }
+
+        return $archiveData;
+    }
+}
