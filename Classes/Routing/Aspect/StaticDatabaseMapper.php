fb828a244a6761045e0095b9cbd919958c91f5b6
Benjamin Kott <benjamin.kott@outlook.com>
2019-05-02 21:50:00 +0200

    [BUGFIX] Correct travis cgl fixer config

--- Classes/Routing/Aspect/StaticDatabaseMapper.php
+++ Classes/Routing/Aspect/StaticDatabaseMapper.php
@@ -1,6 +1,13 @@
 <?php
 declare(strict_types = 1);
 
+/*
+ * This file is part of the package t3g/blog.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace T3G\AgencyPack\Blog\Routing\Aspect;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
