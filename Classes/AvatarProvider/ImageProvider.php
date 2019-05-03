6dadd6fe04e9f08961d77f410c09b1e20fa97b41
Benjamin Kott <benjamin.kott@outlook.com>
2019-05-03 18:50:43 +0200

    [BUGFIX] Respect configured size in Avatar ImageProvider (#74)
    Fixes: #73
    Releases: master, 9.1, 9.0, 8.7

--- Classes/AvatarProvider/ImageProvider.php
+++ Classes/AvatarProvider/ImageProvider.php
@@ -12,7 +12,11 @@ namespace T3G\AgencyPack\Blog\AvatarProvider;
 
 use T3G\AgencyPack\Blog\AvatarProviderInterface;
 use T3G\AgencyPack\Blog\Domain\Model\Author;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Extbase\Domain\Model\FileReference;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Service\ImageService;
 
 /*
  * This file is part of the TYPO3 CMS project.
@@ -43,7 +47,23 @@ class ImageProvider implements AvatarProviderInterface
     {
         $image = $author->getImage();
         if ($image instanceof FileReference) {
-            return $image->getOriginalResource()->getPublicUrl();
+            $defaultSize = 32;
+            $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+
+            $configurationManager = $objectManager->get(ConfigurationManagerInterface::class);
+            $settings = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS, 'blog');
+            $size = ($settings['authors']['avatar']['provider']['size'] ?? $defaultSize) ?: $defaultSize;
+
+            $imageService = $objectManager->get(ImageService::class);
+            $image = $imageService->getImage('', $image, false);
+            $processingInstructions = [
+                'width' => $size . 'c',
+                'height' => $size,
+                'minWidth' => $size,
+                'minHeight' => $size,
+            ];
+            $processedImage = $imageService->applyProcessingInstructions($image, $processingInstructions);
+            return $imageService->getImageUri($processedImage);
         }
         return '';
     }
