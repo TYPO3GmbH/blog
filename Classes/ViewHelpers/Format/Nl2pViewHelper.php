7e4443ea3bc4059f91d7cd3d697e6962bee1e66d
Benjamin Kott <benjamin.kott@outlook.com>
2019-05-04 14:53:28 +0200

    [FEATURE] Format new line to paragraph viewhelper
    Most of the time the format nl2br viewhelper produces
    unwanted and unnessesary line breaks where a paragraph
    is acturally the wanted result. The new viewhelper converts
    linebreaks to actual paragraphs and cleans out empty
    segments. In contrary to the form html viewhelper anything
    else wil be left untouched.
    
    Releases: master

---
+++ Classes/ViewHelpers/Format/Nl2pViewHelper.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package t3g/blog.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\AgencyPack\Blog\ViewHelpers\Format;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
+
+class Nl2pViewHelper extends AbstractViewHelper
+{
+    use CompileWithContentArgumentAndRenderStatic;
+
+    /**
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * @throws \TYPO3Fluid\Fluid\Core\ViewHelper\Exception
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('value', 'string', 'string to format');
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param \TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $data = explode('<br>', nl2br($renderChildrenClosure(), false));
+        $data = array_filter($data, function ($value) {
+            return !empty(trim($value));
+        });
+        return '<p>' . implode('</p><p>', $data) . '</p>';
+    }
+}
