6155665458b08d156e48115bf45d3eb336d5ca8d
Luca Kredel <luca.kredel@web.de>
2019-05-26 19:55:11 +0200

    [BUGFIX] Set default doktype for post model (#83)
    Set the correct doktype via the post model. This is required when posts
    are created via Extbase, in an action or a command for example.
    
    Releases: master, 9.1, 9.0, 8.7, 7.6
    Fixes: #82

--- Classes/Domain/Model/Post.php
+++ Classes/Domain/Model/Post.php
@@ -9,18 +9,7 @@
 
 namespace T3G\AgencyPack\Blog\Domain\Model;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
+use T3G\AgencyPack\Blog\Constants;
 use T3G\AgencyPack\Blog\Domain\Repository\CommentRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
@@ -32,6 +21,13 @@ use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
  */
 class Post extends AbstractEntity
 {
+    /**
+     * The blog post doktype
+     *
+     * @var int
+     */
+    protected $doktype = Constants::DOKTYPE_BLOG_POST;
+
     /**
      * The blog post title.
      *
@@ -149,6 +145,14 @@ class Post extends AbstractEntity
         $this->tags = new ObjectStorage();
         $this->authors = new ObjectStorage();
         $this->media = new ObjectStorage();
+    }
+
+    /**
+     * @return int
+     */
+    public function getDoktype()
+    {
+        return $this->doktype;
     }
 
     /**
