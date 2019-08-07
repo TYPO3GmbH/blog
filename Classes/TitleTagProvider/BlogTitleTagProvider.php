7bd5d949c9bec0e6d1bdcfd0407a10bcc4d1f4c6
Benjamin Kott <benjamin.kott@outlook.com>
2019-08-07 23:25:09 +0200

    [BUGFIX] Use MetaTag API
    Releases: master, 9.1, 9.0

---
+++ Classes/TitleTagProvider/BlogTitleTagProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the package t3g/blog.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\AgencyPack\Blog\TitleTagProvider;
+
+use TYPO3\CMS\Core\PageTitle\AbstractPageTitleProvider;
+
+class BlogTitleTagProvider extends AbstractPageTitleProvider
+{
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+}
