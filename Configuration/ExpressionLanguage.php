a6b6eb237e823065ab79939d87e3ce50eb52333e
Benjamin Kott <benjamin.kott@outlook.com>
2020-01-17 16:27:13 +0100

    [TASK] Add working integration

---
+++ Configuration/ExpressionLanguage.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the package t3g/blog.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'typoscript' => [
+        \T3G\AgencyPack\Blog\ExpressionLanguage\ConditionProvider::class,
+    ]
+];
