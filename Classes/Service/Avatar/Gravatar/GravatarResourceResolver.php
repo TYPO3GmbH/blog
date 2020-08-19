0bd05d3644e20d8bba40715112aa62d048512bc2
Alexander Schnitzler <git@alexanderschnitzler.de>
2020-08-19 15:27:31 +0200

    [FEATURE] Refactor gravatar loading (#161)
    * [FEATURE] Refactor gravatar loading
    
    * [TASK] Do not break existing public api
    
    * [TASK] Remove superfluous CacheManager instance
    
    * [BUGFIX] Reverse if empty logic
    
    * [TASK] Create deriveFileTypeFromContentType method
    
    * [BUGFIX] Use try catch blog instead of null comparison
    
    * [TASK] Introduce extension configuration option
    
    * [TASK] Restructure new files
    
    * [TASK] Add missing dependencies to composer.json
    
    * [BUGFIX] GravatarViewHelper must create author object
    
    * [BUGFIX] Revert argument name
    
    * [TASK] Deactivate DI ¯\_(ツ)_/¯
    
    * [TASK] Do not resolve gravatar urls for comments by default
    
    * [TASK] Code cleanup
    
    * [TASK] Make PR TYPO3 9.5 compatible
    
    * [TASK] Add unit test for GravatarUriBuilder
    
    * [TASK] Add unit test for GravatarResourceResolver
    
    * [BUGFIX] Fix unit test for TYPO3 9.5
    
    * [TASK] Introduce functional test for GravatarProvider
    
    * [TASK] Register functional tests as composer command

---
+++ Classes/Service/Avatar/Gravatar/GravatarResourceResolver.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package t3g/blog.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\AgencyPack\Blog\Service\Avatar\Gravatar;
+
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\UriInterface;
+use T3G\AgencyPack\Blog\DataTransferObject\AvatarResource;
+use T3G\AgencyPack\Blog\DataTransferObject\Gravatar;
+use T3G\AgencyPack\Blog\Service\Avatar\AvatarResourceResolverInterface;
+
+final class GravatarResourceResolver implements AvatarResourceResolverInterface
+{
+    private const HTTP_METHOD = 'GET';
+    private const HTTP_OK_STATUS_CODE = 200;
+
+    /**
+     * @var ClientInterface
+     */
+    private $client;
+
+    /**
+     * @var RequestFactoryInterface
+     */
+    private $requestFactory;
+
+    public function __construct(
+        ClientInterface $client,
+        RequestFactoryInterface $requestFactory
+    ) {
+        $this->client = $client;
+        $this->requestFactory = $requestFactory;
+    }
+
+    /**
+     * @throws ClientExceptionInterface
+     * @throws \RuntimeException
+     */
+    public function resolve(UriInterface $uri): AvatarResource
+    {
+        $request = $this->requestFactory->createRequest(static::HTTP_METHOD, $uri);
+        $response = $this->client->sendRequest($request);
+
+        if ($response->getStatusCode() !== self::HTTP_OK_STATUS_CODE) {
+            throw new \RuntimeException(sprintf(
+                'HTTP request "%s %s" returned non 200 status code "%d %s"',
+                static::HTTP_METHOD,
+                (string)$uri,
+                $response->getStatusCode(),
+                $response->getReasonPhrase()
+            ));
+        }
+
+        $contentType = $response->getHeaderLine('Content-Type');
+        $contentType = !empty($contentType) ? $contentType : 'text/plain';
+
+        $response->getBody()->rewind();
+        return new Gravatar(
+            $uri,
+            $contentType,
+            $response->getBody()->getContents()
+        );
+    }
+}
---
+++ Classes/Service/Avatar/Gravatar/GravatarUriBuilderInterface.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package t3g/blog.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\AgencyPack\Blog\Service\Avatar\Gravatar;
+
+use Psr\Http\Message\UriInterface;
+
+interface GravatarUriBuilderInterface
+{
+    public function getUri(string $email, ?int $size = null, ?string $rating = null, ?string $default = null): UriInterface;
+}
