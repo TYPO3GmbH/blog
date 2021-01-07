48e9e8133065391e1ea3bef48c9aa575a988641c
Benjamin Kott <benjamin.kott@outlook.com>
2021-01-07 11:55:48 +0100

    [BUGFIX] Bundle PSR-Packages for non Composer mode (#181)

---
+++ Resources/Private/PHP/vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5fb0adb5a3ac2db12ca434e4aac86fb099d56468',
+    'name' => 't3g/blog-dependencies',
+  ),
+  'versions' => 
+  array (
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12ac7fcd07e5b077433f5f2bee95b3a771bf61be',
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    't3g/blog-dependencies' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fb0adb5a3ac2db12ca434e4aac86fb099d56468',
+    ),
+  ),
+);
