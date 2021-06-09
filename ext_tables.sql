45546e9836928bb9048955037d377607a2f658b5
Markus Mächler <markus.maechler@bithost.ch>
2021-06-09 11:09:15 +0200

    [BUGFIX] Fix uid_local out of range error when using workspaces

--- ext_tables.sql
+++ ext_tables.sql
@@ -145,10 +145,10 @@ CREATE TABLE tx_blog_domain_model_author (
 # Table structure for table 'tx_blog_post_author_mm'
 #
 CREATE TABLE tx_blog_post_author_mm (
-  uid_local int(11) unsigned DEFAULT '0' NOT NULL,
-  uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-  sorting int(11) unsigned DEFAULT '0' NOT NULL,
-  sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+  uid_local int(11) DEFAULT '0' NOT NULL,
+  uid_foreign int(11) DEFAULT '0' NOT NULL,
+  sorting int(11) DEFAULT '0' NOT NULL,
+  sorting_foreign int(11) DEFAULT '0' NOT NULL,
   KEY uid_local (uid_local),
   KEY uid_foreign (uid_foreign)
 );
