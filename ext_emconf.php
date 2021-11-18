592ac8896d96bd66a521569c66c4808a478dd6f8
Benjamin Kott <benjamin.kott@outlook.com>
2021-11-18 13:13:24 +0100

    [BUGFIX] Correct upper constraint in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '11.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.0.99',
-            'form' => '10.4.0-11.0.99',
+            'typo3' => '10.4.0-11.5.99',
+            'form' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
