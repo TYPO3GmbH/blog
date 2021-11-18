c978937a735b6c5e9416644bd026c7f5b3bc921e
Benjamin Kott <benjamin.kott@outlook.com>
2021-11-18 10:28:00 +0100

    [BUGFIX] Correct dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '11.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.99-11.0.99',
-            'form' => '10.4.99-11.0.99',
+            'typo3' => '10.4.0-11.0.99',
+            'form' => '10.4.0-11.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],
