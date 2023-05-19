5e8851840058178f6be6444eeb139d451ae81b80
Benjamin Kott <benjamin.kott@outlook.com>
2023-05-19 15:36:39 +0200

    [TASK] Cleanup Tag

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,33 +2,10 @@
 # Table structure for table 'tx_blog_domain_model_tag'
 #
 CREATE TABLE tx_blog_domain_model_tag (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) DEFAULT '0' NOT NULL,
-  tstamp int(11) DEFAULT '0' NOT NULL,
-  crdate int(11) DEFAULT '0' NOT NULL,
-  cruser_id int(11) DEFAULT '0' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL,
-  deleted tinyint(4) DEFAULT '0' NOT NULL,
-  hidden tinyint(4) DEFAULT '0' NOT NULL,
-  t3ver_oid int(11) DEFAULT '0' NOT NULL,
-  t3ver_id int(11) DEFAULT '0' NOT NULL,
-  t3_origuid int(11) DEFAULT '0' NOT NULL,
-  t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-  t3ver_label varchar(30) DEFAULT '' NOT NULL,
-  t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-  t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
-  t3ver_count int(11) DEFAULT '0' NOT NULL,
-  t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-  t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-  sys_language_uid int(11) DEFAULT '0' NOT NULL,
-  l18n_parent int(11) DEFAULT '0' NOT NULL,
-  title tinytext,
-  slug varchar(2048),
+  title varchar(255) DEFAULT '' NOT NULL,
+  slug varchar(2048) DEFAULT '' NOT NULL,
   description text,
   content text,
-
-  PRIMARY KEY (uid),
-  KEY parent (pid)
 );
 
 #
