845aae165533dd6369a807e61410f0f9932ef742
Benjamin Franzke <ben@bnf.dev>
2024-10-16 09:56:59 +0200

    [TASK] Replace EMU::addPageTSConfig by Configuration/page.tsconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,15 +19,11 @@ use T3G\AgencyPack\Blog\Notification\Processor\AuthorNotificationProcessor;
 use T3G\AgencyPack\Blog\Routing\Aspect\StaticDatabaseMapper;
 use TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseRowInitializeNew;
 use TYPO3\CMS\Core\Hooks\CreateSiteConfiguration;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
-
-// PageTS
-ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:blog/Configuration/TsConfig/Page/All.tsconfig">');
 
 // Register "blogvh" as global fluid namespace
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['blogvh'][] = 'T3G\\AgencyPack\\Blog\\ViewHelpers';
