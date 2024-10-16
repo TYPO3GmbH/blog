6ff3fa7b2bc0888737b1c5c2da2a5e352fd23770
Benjamin Franzke <ben@bnf.dev>
2024-10-16 11:27:43 +0200

    [TASK] Make AbstractUpdate v13 compatible

--- Classes/Updates/AbstractUpdate.php
+++ Classes/Updates/AbstractUpdate.php
@@ -10,6 +10,7 @@ declare(strict_types = 1);
 
 namespace T3G\AgencyPack\Blog\Updates;
 
+use T3G\AgencyPack\Blog\Updates\Criteria\CriteriaInterface;
 use T3G\AgencyPack\Blog\Updates\Criteria\EqualIntCriteria;
 use T3G\AgencyPack\Blog\Updates\Criteria\EqualStringCriteria;
 use T3G\AgencyPack\Blog\Updates\Criteria\InCriteria;
@@ -119,9 +120,9 @@ abstract class AbstractUpdate
         $queryBuilder->select('*');
         $queryBuilder->from($table);
         if ($condition === self::CONDITION_AND) {
-            $queryBuilder->where(...$criteria);
+            $queryBuilder->where(...array_map(static fn (CriteriaInterface $criterion): string => (string)$criterion, $criteria));
         } else {
-            $queryBuilder->orWhere(...$criteria);
+            $queryBuilder->orWhere(...array_map(static fn (CriteriaInterface $criterion): string => (string)$criterion, $criteria));
         }
 
         $result = $queryBuilder->executeQuery();
