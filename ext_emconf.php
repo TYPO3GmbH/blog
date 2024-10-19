70ab282e60d5d030bed3542db395f14dc62f4360
Benjamin Franzke <ben@bnf.dev>
2024-10-19 10:24:42 +0200

    [TASK] Require at least TYPO3 v13
    Drop v12 support because blog version  v14
    will rely on TYPO3 v13 site sets and drop
    sys_template support.

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.22-13.4.99',
-            'form' => '12.4.22-13.4.99',
+            'typo3' => '13.4.0-13.4.99',
+            'form' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
