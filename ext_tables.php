97e631f563e47decaa1d25e02a8781ad826cb236
Benjamin Kott <benjamin.kott@outlook.com>
2025-06-13 12:43:24 +0200

    [TASK] Remove user.tsconfig fallback

--- ext_tables.php
+++ ext_tables.php
@@ -8,8 +8,6 @@
  */
 
 use T3G\AgencyPack\Blog\Constants;
-use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 if (!defined('TYPO3')) {
     die('Access denied.');
@@ -24,10 +22,3 @@ $GLOBALS['PAGES_TYPES'][Constants::DOKTYPE_BLOG_PAGE] = [
     'type' => 'web',
     'allowedTables' => '*',
 ];
-
-// Replaced with Configuration/user.tsconfig in v13
-if ((new Typo3Version())->getMajorVersion() < 13) {
-    ExtensionManagementUtility::addUserTSConfig('
-        options.pageTree.doktypesToShowInNewPageDragArea := addToList(' . Constants::DOKTYPE_BLOG_POST . ')
-    ');
-}
